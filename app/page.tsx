7abd1f2a734f895778ab8bb7c65c3d9e0e9541cc
Saima-Anwer <saimaanwer681@gmail.com>
2024-10-28 01:31:53 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+import type { Metadata } from 'next';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutUs from './components/AboutUs';
+import Portfolio from "./components/Portfolio";
+import ContactUs from './components/ContactUs';
+
+export const metadata: Metadata = {
+  title: 'My Landing Page',
+  description: 'A minimal landing page',
+};
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <AboutUs />
+        <Portfolio />
+        <ContactUs />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
+
+
+ 
